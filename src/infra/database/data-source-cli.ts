3410d98dd61485bc7825f9a5ae5db54ae055a25b
Igor <igoreduardo20101@gmail.com>
2025-01-27 15:43:54 -0300

    feat: adiciona configuração de banco de dados utilizando migrations

---
+++ src/infra/database/data-source-cli.ts
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const dataSourceOptions: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_DB,
+  synchronize: false,
+  dropSchema: false,
+  logging: false,
+  logger: 'file',
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['src/database/migrations/**/*.ts'],
+  subscribers: ['src/subscriber/**/*.ts'],
+  migrationsTableName: 'migration_table'
+};
+
+const dataSource = new DataSource(dataSourceOptions);
+
+export default dataSource;
